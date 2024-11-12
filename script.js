4c8f823b6471607f5f517ed8f0e8fb5269b7f3ed
esteranodin <esteranodin@hotmail.fr>
2024-11-12 16:38:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log('Hello, world !' + '\n' + 'Bienvenue dans le monde de la programmation');
+
+let nombre1 = 5;
+let nombre2 = 10;
+
+console.log(nombre1 + nombre2);
+
+let tabFruits = ['kiwi', 'fraise', 'mure'];
+for (let fruit = 0; fruit < tabFruits.length; fruit = fruit +1){
+    console.log(tabFruits[fruit]);
+}
+
+for (let a = 0; a < 21; a = a + 1) {
+    if (a )
+}
+    
