a7ae73e65f590a2508fbdf086b5f325a7d867130
esteranodin <esteranodin@hotmail.fr>
2024-11-14 10:39:05 +0100

    ADD corrections

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 
 
 // EXO 2
-// let nombre1 = 5;
-// let nombre2 = 10;
+// const nombre1 = 5;
+// const nombre2 = 10;
 // console.log(nombre1 + nombre2);
 
 
-// EXO 3
+// EXO 3 ==> possible avec forEach / voir correction Jeremy
 // let tabFruits = ['kiwi', 'fraise', 'mure'];
 // for (let fruit = 0; fruit < tabFruits.length; fruit = fruit + 1) {
 //     console.log(tabFruits[fruit]); }
@@ -21,9 +21,9 @@
 // }
 
 
-// EXO 5 boucle dans la boucle
+// EXO 5 boucle dans la boucle // voir avec une focntion sur correction Jeremy
 // const mot = 'bidouille';
-// const voyelle = ['a', 'e', 'i', 'o', 'u'];
+// const voyelle = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
 // let nombreVoyelles = 0;
 
 // for (let i = 0; i < voyelle.length; i = i + 1) {
@@ -36,18 +36,36 @@
 // console.log('Le nombre de voyelles est :' + nombreVoyelles)
 
 
-// EXO 5 sans boucle dans la boucle
+// EXO 5 avec une regEx
 // const mot = 'bidouille';
-// const voyelle = /[aeiou]/g;
+// const voyelle = /[aeiouy]/g;
 // const voyellesFound = (mot.match(voyelle));
 // console.log(voyellesFound.length);
 
-// EXO 6 PAS FINI
+// EXO 6 PAS FINI / VOIR CORRECTION JEREMY avec prog. orientee objet
 // const phrase = 'les poules ont des dents';
 // const caract = /\w/g;
 // const lettresFound = phrase.match(caract);
 // const occurence = /\w/;
 // console.log(lettresFound.search(occurence));
+
+//EXO 6 Correction
+// function countletters(sentence) {
+//     const letterCounts = {};
+//     for(let letter of sentence) {
+//         if(letter.match(/[a-z]/ig)){
+//             letter = letter.tol.owerCase();
+//             if (letterCounts[letter]){
+//                 letterCounts[letter] += 1;
+//             } else {
+//                 letterCounts[letter] = 1;
+//             }
+//         }
+//     }
+//     return letterCounts;
+// }
+// const randomSentence = 'miam le gfdskgd'
+// console.log (countletters(randomSentence));
 
 // EXO 7
 // const tabChiffres = [17, 32, 29, -25, -91];
@@ -65,9 +83,9 @@
 // console.log(nombresPositifs);
 
 // EXO 8
-// let liste5nombres = [56, 87, 78728, 272128, 99];
-// let lePlusGrandNombre = [];
-// for (let i = 0; i < liste5nombres.length; i = i + 1) {
+// const liste5nombres = [56, 87, 78728, 272128, 99];
+// let lePlusGrandNombre = list5nombres[0];
+// for (let i = 1; i < liste5nombres.length; i += 1) {
 //     if (liste5nombres[i] >= lePlusGrandNombre) {
 //         lePlusGrandNombre = liste5nombres[i];
 //     }
